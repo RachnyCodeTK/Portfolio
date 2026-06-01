bc550044f22eb73fbc98e2be893204b5a524b7ed
Lout Rachny <rachnyb.l@gmail.com>
2026-06-01 09:48:35 +0700

    rename footer component to WebFooter to bypass cache

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import Footer from "./components/footer";
+import Footer from "./components/WebFooter";
 
 
 function App() {
